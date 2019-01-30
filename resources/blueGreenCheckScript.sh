57bbdaa77fb7d3942acf834fbcd4a5f6b95478d2
i050674 <i050674@users.noreply.github.com>
2019-01-30 18:14:35 +0100

    Update blueGreenCheckScript.sh

--- resources/blueGreenCheckScript.sh
+++ resources/blueGreenCheckScript.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 # this is simply testing if the application root returns HTTP STATUS_CODE
+echo "MMMMMMMMMM"
+echo "curl -so /dev/null -w '%{response_code}' https://$1 | grep $STATUS_CODE "
+echo "MMMMMMMMMM"
 curl -so /dev/null -w '%{response_code}' https://$1 | grep $STATUS_CODE
